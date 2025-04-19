b3124870df88f45b221cbd9870958cea118faf3d
Moffat <xiaopan0513@sina.com>
2025-04-19 16:36:10 +0800

    增加服务实例

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -13,6 +13,30 @@ module.exports = {
             },
         },
         {
+            name: 'alkanes-api-2',
+            script: 'src/app.js',
+            env: {
+                NODE_ENV: 'dev',
+                port: 20012,
+            },
+            env_pro: {
+                NODE_ENV: 'pro',
+                port: 20012,
+            },
+        },
+        {
+            name: 'alkanes-api-3',
+            script: 'src/app.js',
+            env: {
+                NODE_ENV: 'dev',
+                port: 20013,
+            },
+            env_pro: {
+                NODE_ENV: 'pro',
+                port: 20013,
+            },
+        },
+        {
             name: 'alkanes-job',
             script: 'src/app.js',
             env: {
